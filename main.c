c5b40be1f522620dabccb1b4bd4fe6cff963dace
reecejames254 <158143832+reecejames254@users.noreply.github.com>
2024-02-13 12:55:59 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{   int a = 0;
+    while (a<= 100)
+    {
+        printf("%4d degrees F = %4d degrees C\n",
+               a, (a - 32) * 5 / 9);
+            a = a + 10;
+    }
+    return 0;
+}
