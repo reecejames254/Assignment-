2007d95e7238b123c39fca4444c9f355f52c41bc
reecejames254 <158143832+reecejames254@users.noreply.github.com>
2024-02-13 12:55:08 +0300

    Add files via upload

---
+++ looping/obj/looping example.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 0;
+    while (a <= 100)
+    {
+         printf("%4d degrees F = %4d degrees C\n",
+                a, (a - 32) * 5 / 9);
+            a = a + 1;
+
+    }
+    return 0;
+}
