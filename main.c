c33a9d1d8e53b767030219e913c3824056a71187
reecejames254 <158143832+reecejames254@users.noreply.github.com>
2024-02-14 20:35:53 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{   int a = 0;
-    while (a<= 100)
+int main()
+{
+    float a = 0;
+    while(a <= 100)
     {
-        printf("%4d degrees F = %4d degrees C\n",
-               a, (a - 32) * 5 / 9);
-            a = a + 10;
+        printf("%f kilometers KM = %f miles M C\n",
+               a, (a - 0.0) * 5.0 / 8.0);
+            a = a + 1;
     }
     return 0;
 }
